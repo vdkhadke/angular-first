d70a1be15949543c1ca2a84123f81ae1b5d73990
Viplav K <vdkhadke@users.noreply.github.com>
2019-07-15 19:32:27 -0400

    Add tutorial

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Pixel 3XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone 3A',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Stock Android One',
+    price: 299,
+    description: 'Basic phone with guaranteed android updates'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
